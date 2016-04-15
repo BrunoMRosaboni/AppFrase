e01910e5e2c20e926bbfa85679fbe95d1f563a02
vicks <pedropuzzi@gmail.com>
2016-04-15 18:06:25 -0300

    ueeee

--- hello.py
+++ hello.py
@@ -32,5 +32,6 @@ def gerador():
     return dict(vicks=random.choice(foo))
 
 
-if __name__ == '__main__':
-	app.run()
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
