b8214fa419937bb03ebcb79fc2d6a6716b757300
vicks <pedropuzzi@gmail.com>
2016-04-15 14:10:59 -0300

    oi?

--- hello.py
+++ hello.py
@@ -8,12 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return 
-    "<h1>Hello World</hi> "
-	"<input type="text" name="firstname"><br>"
-
-
-
-
+    "<h1>Hello World</hi>"
+	
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
