c9fe8dfed2737dbdfb0ca28e688bd632b32d47bf
vicks <pedropuzzi@gmail.com>
2016-04-15 17:51:47 -0300

    teste

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
-import os
-from flask import Flask
-
+import os, random
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,7 +7,30 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "<h1>Hello World</hi>"
-	
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+
+@app.route('/vicks')
+def vicks():
+    return 'vicks page!'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.context_processor
+def example():
+    return dict(myexample='This is an example')
+
+@app.context_processor
+def gerador():
+    foo = ['If debugging is the process of removing software bugs, then programming must be the process of putting them in. - Edsger Dijkstra',
+    'One of my most productive days was throwing away 1000 lines of code - Ken Thompson', 
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand. - Martin Fowler', 
+    'When debugging, novices insert corrective code; experts remove defective code. - Richard Pattis', 
+    'Programming is like sex. One mistake and you have to support it for the rest of your life. - Michael Sinz'
+    ]
+    return dict(vicks=random.choice(foo))
+
+
+if __name__ == '__main__':
+	app.run()
---
+++ vicks.py
@@ -0,0 +1,4 @@
+import random
+
+foo = ['Vai toma no cu', 'b', 'c', 'd', 'e']
+print(random.choice(foo))
